919092febce8aa423001b2db65666ac9c9834a42
Antonpavlov <anton.it.pavlov@gmail.com>
2020-09-13 14:58:26 +0300

    домашнее задание 1

---
+++ src/main/java/ru/geekbrains/java/oop/core/hw/Start.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.java.oop.core.hw;
+
+
+import ru.geekbrains.java.oop.core.hw.obstacles.Obstacles;
+import ru.geekbrains.java.oop.core.hw.obstacles.Treadmill;
+import ru.geekbrains.java.oop.core.hw.obstacles.Wall;
+import ru.geekbrains.java.oop.core.hw.participants.Cat;
+import ru.geekbrains.java.oop.core.hw.participants.Human;
+import ru.geekbrains.java.oop.core.hw.participants.Robot;
+import ru.geekbrains.java.oop.core.hw.participants.Runner;
+
+public class Start {
+    public static void main(String[] args) {
+        Runner catTom = new Cat("Tom", 10, 5);
+        Runner catLarry = new Cat("Larry", 7, 3);
+        Runner catBoris = new Cat("Boris", 15, 8);
+
+        Runner humanMax = new Human("Max", 13, 4);
+        Runner humanAnna = new Human("Anna", 10, 2);
+        Runner humanAlex = new Human("Alex", 18, 6);
+
+        Runner robotVally = new Robot("Vally", 13, 4);
+        Runner robotEva = new Robot("Eva", 15, 15);
+        Runner robotBender = new Robot("Bender", 18, 6);
+
+        Runner[] runners = {catTom, catLarry, catBoris, humanMax, humanAnna, humanAlex, robotVally, robotEva, robotBender};
+
+        Obstacles[] obstacles = {
+                new Wall(5),
+                new Wall(6),
+                new Treadmill(10),
+                new Wall(7),
+                new Treadmill(14),
+                new Treadmill(15)
+        };
+
+        System.out.println("Гонка началась!");
+        for (int i = 0; i < obstacles.length; i++) {
+            System.out.println("\n\nИспытание " + (i + 1));
+
+            for (Runner runner : runners) {
+                if (runner.getSuccess()) {
+                    System.out.println("------------");
+                    obstacles[i].check(runner);
+                }
+
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/geekbrains/java/oop/core/hw/obstacles/Obstacles.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java.oop.core.hw.obstacles;
+
+import ru.geekbrains.java.oop.core.hw.participants.Runner;
+
+public interface Obstacles {
+    void check(Runner runner);
+}
