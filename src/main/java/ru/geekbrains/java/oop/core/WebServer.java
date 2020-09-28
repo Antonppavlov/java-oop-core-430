634cd8542626af6260d2503669acd69eb5a8a0fb
Antonpavlov <anton.it.pavlov@gmail.com>
2020-09-28 15:27:44 +0300

    шаг 2

--- src/main/java/ru/geekbrains/java/oop/core/WebServer.java
+++ src/main/java/ru/geekbrains/java/oop/core/WebServer.java
@@ -1,16 +1,43 @@
 package ru.geekbrains.java.oop.core;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class WebServer {
     public static void main(String[] args) throws IOException {
-        System.out.println("start");
+        System.out.println("Наш сервер начал работу");
 
         ServerSocket serverSocket = new ServerSocket(8080);
         Socket accept = serverSocket.accept();
 
-        System.out.println("stop");
+//        Можно считать поток байт
+//        нам нужно преобразовать чтобы мы могли читать текст
+//        accept.getInputStream()
+
+//        Таким мы образом мы могли бы считывать посимвольно
+//        InputStreamReader inputStreamReader = new InputStreamReader(accept.getInputStream(), StandardCharsets.UTF_8);
+//        inputStreamReader.ready();
+//        inputStreamReader.read();
+
+        //Создаем объект BufferedReader который будет накапливать исходящие из InputStreamReader символы
+        //накапливать будет их уже построчно и в формате строки
+
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(accept.getInputStream(), StandardCharsets.UTF_8))) {
+
+            //ждем пока не заполнится строка
+            while (!bufferedReader.ready()) ;
+
+            while (bufferedReader.ready()) {
+                System.out.println(bufferedReader.readLine());
+            }
+
+        }
+
+        System.out.println("Наш сервер закончил работу");
     }
 }
